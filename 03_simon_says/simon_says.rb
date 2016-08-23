0836c1fb309dabef8eee448b45e14e01c22e28be
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-08-23 21:03:49 +0200

    Finish excercise - 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(text)
+  text
+end
+
+def shout(text)
+  text.upcase
+end
+
+def repeat(text, n=2)
+  ((text + ' ') * n).strip
+end
+
+def start_of_word(word, n)
+  word[0...n]
+end
+
+def first_word(word)
+  word.split(' ')[0]
+end
+
+def titleize(word)
+  title = word.split(' ')
+  if title.length == 1 then
+    return title[0].capitalize
+  else
+    title.each_with_index do |t, i|
+      if t.length <= 4 && i > 0 && i < title.length - 1 then next end
+      t.capitalize!
+    end
+    return title.join(' ')
+  end
+end
