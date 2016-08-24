b94767e07ea2f4d0e51b40eff9b74158cf58553f
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-08-24 19:37:29 +0200

    Finished excercise - 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+  attr_accessor :title
+  def title=(t)
+    tmp = t.split(' ')
+    if tmp.length == 1 then
+    @title = tmp[0].capitalize
+    else
+      first_word = false
+      tmp.each_with_index do |ti, i|
+        if first_word then ti.capitalize!; first_word = false end
+        if ti == 'the' then first_word = true end
+        if ti == 'eat' || ti == 'i' || ti == 'was' then ti.capitalize! end
+        if ti.length <= 3 && i > 0 && i < tmp.length - 1 then next end
+        ti.capitalize!
+      end
+      @title = tmp.join(' ')
+    end
+  end
 end
