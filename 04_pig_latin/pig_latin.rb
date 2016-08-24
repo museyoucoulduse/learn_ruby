745d9c6a7715561b44726e63d6e86773edd7adce
Szymon Blaszczynski <museyoucoulduse@icloud.com>
2016-08-24 18:49:34 +0200

    Almost finished excercise 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,41 @@
 #write your code here
+
+def translate(word)
+  sentence = []
+  if [' '].include? word then
+    words = word.split(' ')
+    words.each do |w|
+      sentence << _translate(w)
+    end
+    sentence.join(' ')
+  else
+    _translate(word)
+  end
+end
+
+def _translate(w)
+  if include_vovel(w) then
+    return w = w + 'ay'
+  elsif !(include_vovel(w)) &&
+    !(include_vovel(w, 1)) then
+      ar = w.split ''
+      first = ar.shift
+      second = ar.shift
+      ar = ar << first
+      ar = ar << second
+      return w = ar.join + 'ay'
+  elsif !(include_vovel(w)) then
+    ar = w.split ''
+    first = ar.shift
+    ar = ar << first
+    return w = ar.join + 'ay'
+  end
+end
+
+def include_vovel(w, n=0)
+  if ['a', 'e', 'i', 'o', 'u'].include? w[n] then
+    true
+  else
+    false
+  end
+end
